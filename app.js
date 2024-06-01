73e51575536165fb2521f576b794317a5bb00c31
david123456858 <juandavidperaltafuentes@gmail.com>
2024-05-31 19:05:08 -0500

    Socket

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { createServer } from 'node:http'
 import { connectDB } from './src/database/db.js'
 import routerOrdenes from './src/router/Ordenes.js'
 import { config } from 'dotenv'
-// import infoSocket from './src/Model/informacion.js'
+import infoSocket from './src/Model/informacion.js'
 
 config()
 const app = express()
@@ -35,35 +35,35 @@ app.use(routerOrdenes)
 app.get('/', (req, res) => {
   res.send('Bienvenido a ordenes DevelopWord')
 })
-// app.get('/socket', (req, res) => {
-//   res.sendFile(process.cwd() + '/Client/index.html')
-// })
+app.get('/socket', (req, res) => {
+  res.sendFile(process.cwd() + '/Client/index.html')
+})
 
-// io.on('connection', (socket) => {
-//   let persona = socket.handshake.query.usuario
-//   if (persona !== undefined && persona !== null) {
-//     persona = JSON.parse(persona)
-//     infoSocket.find({
-//       idAdmin: persona.id_usuario
-//     }).then((notificaciones) => {
-//       socket.emit('notificaciones', notificaciones)
-//     }).catch((error) => {
-//       console.error('Error al obtener notificaciones:', error)
-//     })
-//   } else {
-//     console.log('No hay usuario')
-//   }
-//   socket.on('nuevaNotificacion', (mensaje) => {
-//     const nuevaNotificacion = new infoSocket(mensaje)
-//     nuevaNotificacion.save().then(() => {
-//       io.emit('nuevaNotificacion', nuevaNotificacion)
-//     })
-//   })
+io.on('connection', (socket) => {
+  let persona = socket.handshake.query.usuario
+  if (persona !== undefined && persona !== null) {
+    persona = JSON.parse(persona)
+    infoSocket.find({
+      idAdmin: persona.id_usuario
+    }).then((notificaciones) => {
+      socket.emit('notificaciones', notificaciones)
+    }).catch((error) => {
+      console.error('Error al obtener notificaciones:', error)
+    })
+  } else {
+    console.log('No hay usuario')
+  }
+  socket.on('nuevaNotificacion', (mensaje) => {
+    const nuevaNotificacion = new infoSocket(mensaje)
+    nuevaNotificacion.save().then(() => {
+      io.emit('nuevaNotificacion', nuevaNotificacion)
+    })
+  })
 
-//   socket.on('disconnect', () => {
-//     console.log('Usuario desconectado')
-//   })
-// })
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado')
+  })
+})
 
 server.listen(port, () => {
   console.log(`listo para utilizar http://localhost:${port}`)
